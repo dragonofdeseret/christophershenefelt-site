06260e68270c88be20240a10fd8ccf2d315d6fd2
dragonofdeseret <daodezion@icloud.com>
2026-03-12 14:24:09 -0600

    js patch

--- artworks.js
+++ artworks.js
@@ -3,14 +3,14 @@ const artworks = [
 {
 title: "February 14, 2022",
 year: "2022",
-image: "images/2022-02-14.jpg",
+image: "../images/art/2022/2022-02-14.jpg",
 page: "art/2022-02-14.html"
 },
 
 {
 title: "January 30, 2022",
 year: "2022",
-image: "images/2022-01-30.jpg",
+image: "../images/art/2022/2022-01-30.jpeg",
 page: "art/2022-01-30.html"
 }
 
